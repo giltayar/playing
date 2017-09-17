a9ae2b4917b1adb6ee4876e6d044850cd133074e
Gil Tayar <gil@tayar.org>
2017-09-17 09:48:42 +0300

    added selenium to visual testing

--- javascript/visual-testing/test/selenium-webdriver.test.js
+++ javascript/visual-testing/test/selenium-webdriver.test.js
@@ -1,21 +1,30 @@
 'use strict'
-const {promisify: p} = require('util')
 const {describe, it, before, after} = require('mocha')
 const {expect} = require('chai')
 const webdriver = require('selenium-webdriver')
+const {Eyes} = require('eyes.selenium')
+const app = require('../src/sample-web-app')()
 require('chromedriver')
 require('geckodriver')
 const {By, until} = webdriver
-const app = require('../src/sample-web-app')()
 
 describe('sample web app', function() {
   let server
   let driver
+  let eyes
   before(done => (server = app.listen(process.env.PORT || undefined, done)))
   after(() => server.close())
 
   before(() => (driver = new webdriver.Builder().forBrowser('chrome').build()))
   after(async () => await driver.quit())
+
+  before(async () => {
+    eyes = new Eyes()
+    if (!process.env.APPLITOOLS_APIKEY) throw new Error('Set APPLITOOLS_APIKEY env var to your key')
+    eyes.setApiKey(process.env.APPLITOOLS_APIKEY)
+    await eyes.open(driver, 'Samp', 'Sample Web App Test', {width: 800, height: 600})
+  })
+  after(async () => await eyes.close())
 
   const baseUrl = () => `http://localhost:${server.address().port}`
 
@@ -25,6 +34,8 @@ describe('sample web app', function() {
     await driver.wait(until.elementLocated(By.tagName('h1')))
 
     expect(await (await driver.findElement(By.tagName('h1'))).getText()).to.equal('Hello, world')
+
+    await eyes.checkWindow('Hello world appears')
   })
 
   it('should fill dismiss alert', async () => {
