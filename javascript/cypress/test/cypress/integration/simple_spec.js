eed4c126405f1114b7d05cd1c727c4124f5cbcae
Gil Tayar <gil@tayar.org>
2018-05-29 16:36:21 +0300

    cypress changes

--- javascript/cypress/test/cypress/integration/simple_spec.js
+++ javascript/cypress/test/cypress/integration/simple_spec.js
@@ -4,12 +4,15 @@ const {describe, it} = require('mocha')
 describe('Blog App', function () {
   it('should do stuff', () => {
     const random = '1' // `${Math.random() * 100000 | 0}`
+
+    // Visit registration page
     cy.visit('http://localhost:3000/register')
 
-    cy.eyesOpen('blog app', 'blog app cypress for Kent C. Dodds', {width: 1024, height: 768})
+    cy.eyesOpen('blog app', 'blog app cypress', {width: 1024, height: 768})
 
-    cy.eyesCheckWindow('register')
+    cy.eyesCheckWindow('register') // visual snapshot
 
+    // Register user
     cy.get(':nth-child(3) > .nav-link').click()
 
     cy.get('input[placeholder="Username"]').type(`user${random}`)
@@ -19,12 +22,13 @@ describe('Blog App', function () {
     cy.get('.btn').click()
 
     cy.contains('New Post')
-    cy.eyesCheckWindow('home page after register')
+    cy.eyesCheckWindow('home page after register') // visual snapshot
 
+    // Create new blog post
     cy.contains('New Post').click()
 
     cy.get('input[placeholder="Article Title"]')
-    cy.eyesCheckWindow('new post page')
+    cy.eyesCheckWindow('new post page') // visual snapshot
 
     cy.get('input[placeholder="Article Title"]').type('A great blog post!')
     cy.get('input[placeholder="What\'s this article about?"]').type("It's a post about nothing")
@@ -32,20 +36,22 @@ describe('Blog App', function () {
 
     cy.get('.btn').click()
 
+    // Add comment to blog post
     cy.get('.card-footer > .btn')
-    cy.eyesCheckWindow('published blog post')
+    cy.eyesCheckWindow('published blog post') // visual snapshot
 
     cy.get('.form-control').type('Trolling... Trolling!')
     cy.get('.card-footer > .btn').click()
 
     cy.get('.card-text')
-    cy.eyesCheckWindow('blog post with comment')
+    cy.eyesCheckWindow('blog post with comment') // visual snapshot
 
+    // View all blog posts
     cy.get('.navbar-brand').click()
     cy.get('.feed-toggle > .nav > :nth-child(2) > .nav-link').click()
 
     cy.get('.preview-link > p')
-    cy.eyesCheckWindow('home page with blog post')
+    cy.eyesCheckWindow('home page with blog post') // visual snapshot
 
     cy.eyesClose()
   })
