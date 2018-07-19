75cb935fc6434130ac935169bf3064c78a127bd7
Gil Tayar <gil@tayar.org>
2018-07-19 13:07:21 +0300

    stuff

--- javascript/foxdriver/src/foxdriver.js
+++ javascript/foxdriver/src/foxdriver.js
@@ -1,5 +1,17 @@
 'use strict'
+const Foxdriver = require('foxdriver')
 
-module.exports = {
-  //
+async function main() {
+  const {browser} = await Foxdriver.launch({
+    url: 'https://en.wikipedia.org/wiki/Main_Page',
+  })
+
+  // wait until page is loaded
+  await new Promise(resolve => setTimeout(resolve, 3000))
+
+  await browser.device.screenshotToFile(__dirname + '/screenshot.png')
+
+  await browser.close()
 }
+
+main().catch(console.error)
