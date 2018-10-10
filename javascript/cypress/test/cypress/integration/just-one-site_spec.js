11059ff62350525f6728657fb1d74ca4fff75d35
Gil Tayar <gil@tayar.org>
2018-10-10 17:17:34 +0300

    asdfadf

--- javascript/cypress/test/cypress/integration/just-one-site_spec.js
+++ javascript/cypress/test/cypress/integration/just-one-site_spec.js
@@ -1,7 +1,8 @@
 'use strict'
 const {describe, it} = require('mocha')
 
-const urlToCheck = `https://www.amazon.com/All-new-Echo-Dot-3rd-Gen/dp/B0792R1RSN/ref=redir_mobile_desktop`
+// const urlToCheck = `https://www.amazon.com/All-new-Echo-Dot-3rd-Gen/dp/B0792R1RSN/ref=redir_mobile_desktop`
+const urlToCheck = `http://snapshot-server-app/All-new-Echo-Dot-3rd-Gen/dp/B0792R1RSN/ref=redir_mobile_desktop?rg_render-path=%2Fsha256%2F0b1fc56ad0bbfbdb6724fab5bca3f426350da39fa6b2733c00a7357c89fad7ce&rg_render-id=91d2603b-6c13-44d0-8e9f-13e918556bca&rg_auth-token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0SnlGZmMwaFRVX2pzZTVCNlFTX2lnfn4iLCJpYXQiOjE1MzkxNjMxOTIsImV4cCI6MTUzOTE4NDc5MiwiaXNzIjoiZXllc2FwaS5hcHBsaXRvb2xzLmNvbSJ9.ntBg7IJJ18R7OkrPJsWbsvJRd10iDpIyWADwEluWzFth_sHy-6le-66RTdFz1-qXcsujdwr8jQQCaZeLp8WV75mIrvo-AM93rYoybZrh5wMvZYv9Y5imcYx4GK4vF-N-nBDRjj6W_9cmScMzmJjefJImSLYiIUFqvsCLE3oxtAI&rg_origin=https%3A%2F%2Fwww.amazon.com`
 
 describe(`URL: ${urlToCheck}`, function () {
   it(urlToCheck, () => {
@@ -16,7 +17,7 @@ describe(`URL: ${urlToCheck}`, function () {
 
     cy.visit(urlToCheck)
 
-    cy.eyesCheckWindow(urlToCheck)
+    cy.eyesCheckWindow(urlToCheck).then(console.log('@@@GIL checkedwindow!'))
 
     cy.eyesClose()
   })
--- javascript/puppeteer/tests/puppeteer/play-around.js
+++ javascript/puppeteer/tests/puppeteer/play-around.js
@@ -2,11 +2,11 @@
 const puppeteer = require('puppeteer')
 
 async function main() {
-  // const browser = await puppeteer.launch({headless: true})
-  const browser = await puppeteer.connect({browserWSEndpoint: `ws://0.0.0.0:33068/devtools/browser/9b5997cf-9a47-4efd-9a71-7621306a56c1`})
+  const browser = await puppeteer.launch({headless: true})
+  // const browser = await puppeteer.connect({browserWSEndpoint: `ws://0.0.0.0:33068/devtools/browser/9b5997cf-9a47-4efd-9a71-7621306a56c1`})
 
   const page = await browser.newPage()
-  await page.goto('https://www.amazon.com/All-new-Echo-Dot-3rd-Gen/dp/B0792R1RSN/ref=redir_mobile_desktop')
+  await page.goto('https://www.amazon.com/All-new-Echo-Dot-3rd-Gen/dp/B0792R1RSN/ref=redir_mobile_desktop', {waitUntil: 'domcontentloaded'})
 
   console.log('taking screenshot...')
   await page.screenshot({path: `screenshot.png`, fullPage: true})
@@ -15,6 +15,7 @@ async function main() {
   //   await page.screenshot({path: `screenshot-${i + 1}.png`, clip: {x: 0, y: 5000 * i, width: 1018, height: i === 0 ? 5000 : 9891 - 5000} })
   // }
   console.log('took screenshot')
+  process.exit(0)
 }
 
 main().catch(console.error)
